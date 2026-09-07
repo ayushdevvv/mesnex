9f9a6a7a91f9a76b818cd327b1113156c6b534ed
AYUSH <dixitayush6394@gmail.com>
2026-09-07 17:46:41 +0530

    UI fix

--- frontend/src/pages/AuthPage.jsx
+++ frontend/src/pages/AuthPage.jsx
@@ -7,10 +7,15 @@ function AuthPage() {
     <div className="min-h-dvh w-full bg-background text-foreground md:h-dvh md:overflow-hidden">
       <AuthHeader />
 
-      <main className="relative flex min-h-[calc(100dvh-64px)] flex-col md:h-[calc(100dvh-64px)] md:flex-row">
-        <AuthHeroPanel />
-        <AuthActionPanel />
-      </main>
+    <main className="relative flex min-h-[calc(100dvh-64px)] flex-col md:h-[calc(100dvh-64px)] md:flex-row">
+  <div className="order-2 md:order-1 md:flex-1">
+    <AuthHeroPanel />
+  </div>
+
+  <div className="order-1 md:order-2 md:flex-1">
+    <AuthActionPanel />
+  </div>
+</main>
     </div>
   );
 }
